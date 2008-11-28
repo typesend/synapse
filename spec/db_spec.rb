2d8d86e5b00ab6f09eff8cf2a1e50aa1fcf45786
Sho Fukamachi <sho@fukamachi.org>
2008-11-29 06:40:33 +1100

    oops - users.yaml should not be in there, deleted

--- spec/db_spec.rb
+++ spec/db_spec.rb
@@ -1,14 +1,18 @@
 require File.join(File.dirname(__FILE__), %w[spec_helper])
 
+# in this spec file we are going to test loading and dumping users and user relationships from the database.
+
+include DB
 
 
-
-describe DB, 'synapse db' do
+describe DB, 'a user being modified' do
   before(:each) do
-    #@e = Entity.test_generate
+    @user = User.new('unit', 'example.org', 'secret')
   end
   
-
+  it "should set its record to dirty" do
+    
+  end
   
 end
 
